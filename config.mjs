4cd7b09422ae26e8000c1ce88d75cc18cc6db54b
4lno <lvano.l0346@gmail.com>
2025-10-07 23:25:34 +0700

    Update config.mjs

--- config.mjs
+++ config.mjs
@@ -1,4 +1,4 @@
-export const WEBSOCKET_PUBLIC_URL = "http://localhost:5505/"
+export const WEBSOCKET_PUBLIC_URL = "wss://rahoot-ws-4gel.onrender.com/"
 export const WEBSOCKET_SERVER_PORT = 5505
 
 const QUIZZ_CONFIG = {
